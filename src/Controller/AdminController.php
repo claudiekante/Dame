37585c98c5b1e4dfbcc52dc9240939b7533c418a
claudiekante <claudiekante@gmail.com>
2023-01-11 15:47:13 +0100

    Page Admin coded

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\JourneyRepository;
+use App\Repository\LegRepository;
+use App\Repository\StopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +14,15 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/dashboard", name="admin_dashboard")
      */
-    public function index(): Response
+    public function index(StopRepository $stopRepository, LegRepository $legRepository, JourneyRepository $journeyRepository): Response
     {
+        $stopsList = $stopRepository->findAll();
+        $legsList = $legRepository->findAll();
+        $journeysList = $journeyRepository->findAll();
         return $this->render('admin/pageAdmin.html.twig', [
+            "stopsList"=>$stopsList,
+            "legsList"=>$legsList,
+            "journeysList"=>$journeysList,
             'controller_name' => 'AdminController',
         ]);
     }
